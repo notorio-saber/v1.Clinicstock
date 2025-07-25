6c0817a04095f61f468c85a40a950900ed6b7b65
Jonathan Felipe Costa <jonathanfelipebc@gmail.com>
2025-07-25 22:52:46 +0000

    Maravilha! deu certo! Agora atualizou a foto de perfil!
    - só um pequeno

--- src/app/(app)/_components/header.tsx
+++ src/app/(app)/_components/header.tsx
@@ -1,19 +1,30 @@
+'use client';
+
 import Logo from '@/components/logo';
 import { ThemeToggle } from '@/components/theme-toggle';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import Link from 'next/link';
+import useAuth from '@/hooks/useAuth';
 
 export default function Header() {
+  const { user } = useAuth();
+
   return (
     <header className="fixed top-0 left-0 right-0 z-10 flex h-16 items-center justify-between border-b bg-secondary px-4 shadow-sm">
       <Logo />
       <div className="flex items-center gap-4">
         <ThemeToggle />
         <Link href="/profile">
-            <Avatar className="h-9 w-9 cursor-pointer">
-              <AvatarImage src="https://placehold.co/40x40.png" alt="Avatar" data-ai-hint="profile person" />
-              <AvatarFallback>CE</AvatarFallback>
-            </Avatar>
+          <Avatar className="h-9 w-9 cursor-pointer">
+            <AvatarImage
+              src={user?.photoURL || undefined}
+              alt="Avatar"
+              data-ai-hint="profile person"
+            />
+            <AvatarFallback>
+              {user?.displayName?.charAt(0) || user?.email?.charAt(0) || 'U'}
+            </AvatarFallback>
+          </Avatar>
         </Link>
       </div>
     </header>
