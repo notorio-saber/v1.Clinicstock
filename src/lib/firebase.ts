35b775b0bd1c6b2a8723648ef96d756511e7fde8
Jonathan Felipe Costa <jonathanfelipebc@gmail.com>
2025-07-25 21:03:20 +0000

    agora o app nem abre no navegador! estamos piorando a cada passo!
    essa

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore, Firestore } from 'firebase/firestore';
 import { getStorage, FirebaseStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyB0ae8Sx_tquzgYaukQj_S4YFNj-RMXbX8",
+  authDomain: "clinicstock.firebaseapp.com",
+  projectId: "clinicstock",
+  storageBucket: "clinicstock.appspot.com",
+  messagingSenderId: "148870614912",
+  appId: "1:148870614912:web:be87f6b3b8ecb599c33b65"
 };
 
 // Initialize Firebase only on the client side
