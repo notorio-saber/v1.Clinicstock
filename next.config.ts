ac4df15c18cdda6ba62f2a7fc83278f21f2930fb
Jonathan Felipe Costa <jonathanfelipebc@gmail.com>
2025-07-29 02:35:17 +0000

    ainda está redirecionando para o auth! e no momento que abre o modal do

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,23 @@
 import type { NextConfig } from 'next';
 
 const config: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      },
+    ];
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
